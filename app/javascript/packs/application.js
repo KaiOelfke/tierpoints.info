593e12581b911fd7176e0e18016a40ff76ac1d39
Kai Oelfke <k.oelfke@me.com>
2019-04-08 15:31:11 +0800

    Remove crisp chat, add feedback button

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -84,10 +84,10 @@ import "flatpickr/dist/flatpickr.min.css";
 
 import Chat from 'chat'
 
-document.addEventListener('turbolinks:load', () => {
-  window.$crisp=[];
-  window.CRISP_COOKIE_EXPIRE = 3600;
-  window.CRISP_WEBSITE_ID="6da2b682-7013-4e7d-80f4-906b35c27f6a";
-  Chat.addChat();
-});
+// document.addEventListener('turbolinks:load', () => {
+//   window.$crisp=[];
+//   window.CRISP_COOKIE_EXPIRE = 3600;
+//   window.CRISP_WEBSITE_ID="6da2b682-7013-4e7d-80f4-906b35c27f6a";
+//   Chat.addChat();
+// });
 
